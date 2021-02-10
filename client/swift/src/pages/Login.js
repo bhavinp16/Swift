e5d0eeadeeba2903c37f17112a0c83fa8b99b249
Bhavin <bhavin1625p@gmail.com>
2021-02-10 10:36:03 +0530

    login and signup page stylled

--- client/swift/src/pages/Login.js
+++ client/swift/src/pages/Login.js
@@ -40,17 +40,17 @@ function Login() {
     }
 
     return (
-        <div className="flex flex-col items-center h-screen w-screen bg-gray-800 justify-center">
-            <div className="h-3/4 w-1/2 flex flex-col items-center justify-center bg-gray-200">
-                <h1 className="mb-12 text-3xl font-serif "> Swift </h1>
+<div className="flex flex-col items-center h-screen w-screen bg-gray-800 justify-center text-gray-700">
+            <div className="h-3/4 w-1/2 flex flex-col items-center justify-center bg-gray-100 ring-8 shadow-lg ring-green-100 border-4 border-gray-800 rounded">
+                <h1 className="mb-12 text-3xl font-serif"> Swift </h1>
                 <form className="flex flex-col justify-center text-lg w-1/2">
                     <label htmlFor="Eid">Email Id</label>
-                    <input className="p-1 pl-4 rounded-lg" type="email" name="email" id="Eid" onChange={handlechange} />
+                    <input className="p-1 pl-4 rounded-lg shadow-md" type="email" name="email" id="Eid" onChange={handlechange} />
 
                     <label htmlFor="pid" className="mt-3">Password</label>
-                    <input className="p-1 pl-4 rounded-lg" type="password" name="password" id="pid" onChange={handlechange} />
+                    <input className="p-1 pl-4 rounded-lg shadow-md" type="password" name="password" id="pid" onChange={handlechange} />
 
-                    <button className="hover:bg-yellow-200 bg-yellow-100 m-8 mb-4 text-yellow-700 rounded-lg h-10 " onClick={logind}> Login </button>
+                    <button className="hover:bg-green-200 bg-green-100 m-8 mb-4 text-green-700 rounded-lg h-10 shadow-lg" onClick={logind}> Login </button>
 
                     <Link to="/signup" className="flex justify-center text-gray-500 hover:text-gray-900 font-serif">Create an account</Link>
                 </form>
